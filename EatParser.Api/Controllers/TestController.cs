d4594e4c89b9c5d529411c21c4112772c7456e0e
h17l2y1 <vladislavvillya@gmail.com>
2019-07-09 21:38:23 +0300

    test sync

---
+++ EatParser.Api/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EatParser.Api.Controllers
+{
+	[Route("api/[controller]/[action]")]
+	[ApiController]
+	public class TestController : ControllerBase
+	{
+		private readonly IService _service;
+
+		public TestController(IService service)
+		{
+			_service = service;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> StartGame(string prop1, string prop2)
+		{
+			var view = await _service.Start(prop1, prop2);
+			return Ok(view);
+		}
+	}
+}
