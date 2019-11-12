e744255cb5ac9b6ae794d4835453f528bdff25e1
vlad <vlad.h17l2y1@.com>
2019-11-12 13:14:10 +0200

    feat(Angular): setup lazy loading

--- EatParser.Web.Angular/EatParserAngular/src/app/app-routing.module.ts
+++ EatParser.Web.Angular/EatParserAngular/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) },
+  { path: 'login', loadChildren: () => import('./auth/login/login.module').then(m => m.LoginModule) },
+  { path: 'menu', loadChildren: () => import('./pages/main-menu/main-menu.module').then(m => m.MainMenuModule) }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ EatParser.Web.Angular/EatParserAngular/src/app/auth/login/login.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from './login-routing.module';
+
+@NgModule({
+  declarations: [
+    LoginComponent
+  ],
+  imports: [
+    CommonModule,
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    FormsModule,
+  ]
+})
+export class LoginModule { }
