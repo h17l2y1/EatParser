bb88abc5b0bf713dfeaeabe19091eefb424189d4
vlad <vlad.h17l2y1@.com>
2019-11-12 13:51:34 +0200

    temp(Angular): temp comment

--- EatParser.Web.Angular/EatParserAngular/src/app/auth/login/login.component.ts
+++ EatParser.Web.Angular/EatParserAngular/src/app/auth/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  public loginForm: FormGroup;
+
+  constructor(private router: Router) { }
 
   ngOnInit() {
+    this.initLoginForm();
+  }
+
+  private initLoginForm(): void {
+    this.loginForm = new FormGroup({
+      login: new FormControl(''),
+      password: new FormControl(''),
+    });
+  }
+
+  public getLoginData(): void {
+    const aaa = this.loginForm.controls.login.value;
+    const bbb = this.loginForm.controls.password.value;
   }
 
 }
