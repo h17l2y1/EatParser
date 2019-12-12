86dd8bddaf90ca8c45166cae902993f52416ef75
vlad <vlad.h17l2y1@gamil.com>
2019-12-12 13:49:23 +0200

    fear(ViewModel): add view models

---
+++ EatParser.ViewModels/Set/GetAllSetView.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace EatParser.ViewModels.Set
+{
+	public class GetAllSetView
+	{
+		public List<GetAllSetViewItem> Sets { get; set; }
+
+		public GetAllSetView(List<GetAllSetViewItem> set)
+		{
+			Sets = set;
+		}
+
+	}
+
+	public class GetAllSetViewItem : ProductView
+	{
+
+	}
+}
